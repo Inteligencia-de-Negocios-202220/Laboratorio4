fbfd8ec3ae128a5cdfd20a23c3a9ea726fa57fe3
ElReyZero <juan.romero1201@gmail.com>
2022-10-25 06:42:34 -0500

    Feat: First Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from ml_model import Model
+from fastapi import FastAPI
+from data_model import DataModel
+import pandas as pd
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+   return {"Hello": "World"}
+
+@app.post("/fit")
+def fit(data: list[DataModel]):
+   data = pd.DataFrame(data)
+   model = Model()
+   result = model.fit(data)
+   return result
+
+@app.post("/predict")
+def make_predictions(dataModel: list[DataModel]):
+   model = Model()
+   resultList = list()
+   for element in dataModel:
+      df = pd.DataFrame(element.dict(), columns=element.dict().keys(), index=[0])
+      df.columns = element.columns()
+      result = model.predict(df)
+      resultList.append(result[0])
+   return {"results": resultList }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ ml_model.py
@@ -0,0 +1,43 @@
+from joblib import load
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+
+class Model:
+
+    def __init__(self):
+        self.features = ['GRE Score', 'TOEFL Score', 'University Rating', 'SOP', 'LOR ', 'CGPA', 'Research']
+        self.pipeline = Pipeline(
+            [
+                ('feature_selection', ColumnTransformer(
+                    [
+                        ('selector', 'passthrough', self.features),
+                        ('poly', PolynomialFeatures(degree = 2, include_bias = False), ['GRE Score', 'TOEFL Score', 'SOP', 'LOR ', 'CGPA'])
+                    ]
+                )),
+                ('scaler', StandardScaler()),
+                ('model', LinearRegression())
+            ]
+        )
+        self.model = load("assets/modelo.joblib")
+
+    def fit(self, data):
+        data.dropna(subset=['Admission Points'], inplace=True)
+        X_train, X_test, Y_train, Y_test = train_test_split(data, data["Admission Points"], test_size=0.2, random_state=1)
+        error_dict = {
+            "Mean_Absolute_Error_Train": mean_absolute_error(Y_train, self.pipeline.predict(X_train)),
+            "Root_Mean_Squared_Error_Train": mean_squared_error(Y_train, self.pipeline.predict(X_train)),
+            "Mean_Absolute_Error_Test": mean_absolute_error(Y_test, self.pipeline.predict(X_test)),
+            "Root_Mean_Squared_Error_Test": mean_squared_error(Y_test, self.pipeline.predict(X_test)),
+        }
+        return error_dict
+
+    def predict(self, data):
+        result = self.model.predict(data)
+        return result
